c8b8321c36c70ac41e2ac79986bed89325db307a
Rita Tang <doggymint@gmail.com>
2023-09-04 16:37:48 -0400

    Add POST route for urls and add generateRandomString function

--- express_server.js
+++ express_server.js
@@ -2,7 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = () => {
+  // generate randomstring with length of 6 using base 36;
+  const randomString = Math.random().toString(36).slice(6);
+  let returnString = "";
+  for (const s of randomString) {
+    // when it is not a number, meaning it's a string
+    if (isNaN(s) && Math.random() < 0.5) {
+      // convert the char to capital letter with a 50% chance
+      returnString += s.toUpperCase();
+    } else {
+      returnString += s;
+    }
+  }
+  return returnString;
+};
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -18,6 +35,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  // const longURL = req.body.longURL;
+  // console.log(longURL);
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -29,7 +53,6 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
