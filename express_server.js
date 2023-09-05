1a11668eb6b30ee9717fe049e91ca334fcdb092c
Rita Tang <doggymint@gmail.com>
2023-09-05 18:42:13 -0400

    Add /login GET route

--- express_server.js
+++ express_server.js
@@ -89,6 +89,13 @@ app.post("/register", (req, res) => {
 });
 
 /* ------- "/login" ------ */
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("login", templateVars);
+});
+
 app.post("/login", (req, res) => {
   const { username } = req.body;
   res.cookie("username", username);
