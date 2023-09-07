eb72524f113191ab7f5b037b9c152c071fd3969d
Rita Tang <doggymint@gmail.com>
2023-09-07 13:06:31 -0400

    Modify urlDatabase structure

--- data.js
+++ data.js
@@ -1,8 +1,15 @@
 const urlDatabase = {
-  /*  =====  format  =====
+  /* =====  format  =====
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userID: "userRandomID",
+    createdAt: "9/7/2023",
+    visits: [
+      {
+        timestamp: "Thu Sep 07 2023 12:43:12 GMT-0400 (Eastern Daylight Time)",
+        visitor_id: "2wkR52",
+      },
+    ],
   },
   */
 };
